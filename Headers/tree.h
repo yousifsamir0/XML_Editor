a884cf155d1dd63ca4d066038916ffa2ce1a38e0
fadyNabil16 <68451496+fadyNabil16@users.noreply.github.com>
2021-07-14 17:08:02 +0200

    Create tree.h

---
+++ Headers/tree.h
@@ -0,0 +1,70 @@
+#ifndef TREE_H
+#define TREE_H
+#include <vector>
+#include <string>
+#include<stack>
+#include<new>
+using namespace std;
+class node
+{
+private:
+    vector<node*> children;
+    string tag_name;
+    bool is_tag;
+    bool is_valid;
+    bool have_data;
+    string err_tag, correct_tag;
+    string attr;
+    string data;
+    bool self_close;
+    bool comment;
+    bool state;
+
+public:
+    node(string name, bool is_tag, bool self_close_, bool comment_);
+    ~node();
+    void addChild(node* child);
+    bool isTag();
+    void addAtrr(string attrbiute);
+    string getAttr();
+    void setValid(bool statue);
+    void setHaveData();
+    bool getHaveData();
+    void setState(bool state);
+    bool getState();
+    void setEndTag(string tag);
+    string getEndTag();
+    void setCorrectTag(string tag);
+    string getCorrectTag();
+    vector<node*> getChild();
+    string tagName();
+    bool getIsComment();
+    bool getIsSelfClose();
+
+    friend class XML_Parser;
+};
+
+class Tree {
+private:
+    node* root;
+public:
+    Tree()
+    {
+        root = NULL;
+    }
+    ~Tree()
+    {
+        delete this;
+    }
+    void setRoot(node* ptr)
+    {
+        this->root = ptr;
+    }
+    node* getRoot() {
+        return this->root;
+    }
+    void allocate_tage_to_parent(stack<node*>* xmlTags, string tag);
+    void data_node(stack<node*>* xmlTags, node* data, bool state);
+    void parser(string rawXml); // function to call
+    void minify(string rawXml);
+};
